5da5320951bda78994261fd019cee1cc68560813
hemomona <18146856052@163.com>
2026-05-27 15:55:02 +0800

    codex 3rd: rollback for matplotlib

--- src/memxterminator/__init__.py
+++ src/memxterminator/__init__.py
@@ -1,3 +1,14 @@
+from __future__ import annotations
+
+import warnings
+
 __author__ = 'Zhen Huang'
 __email__ = 'zhen.victor.huang@gmail.com'
 __version__ = '1.2.2'
+
+
+warnings.filterwarnings(
+    "ignore",
+    message=r"pkg_resources is deprecated as an API.*",
+    category=UserWarning,
+)
